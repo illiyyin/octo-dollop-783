80f1ed8bf71c2f0ee116c840244aba4d1d264909
illiyyin <iilliyyin@gmail.com>
2022-09-29 08:43:06 +0700

    route & rough layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Billing from "./pages/Billing";
+import Dashboard from "./pages/Dashboard";
+import Tables from "./pages/Tables";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <h1 className="text-3xl font-bold  text-red-600">
-      Hello world!
-    </h1>
-  )
+	return (
+		<>
+			<div className="flex h-full">
+				<Sidebar />
+				<div>
+					<Navbar />
+					<Routes>
+						<Route path="/dashboard" element={<Dashboard />} />
+						<Route path="/tables" element={<Tables />} />
+						<Route path="/billing" element={<Billing />} />
+						<Route path="" element={<Navigate to="/dashboard" />} />
+					</Routes>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
